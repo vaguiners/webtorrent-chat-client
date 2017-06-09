6e3b403947bbf41cf279ae3f8906d7562cbfdf3b
Filip Waligórski <filip.waligorski@student.put.poznan.pl>
2017-06-09 15:53:52 +0200

    removed not working code - redirecting to #! not working

--- web.js
+++ web.js
@@ -5,7 +5,4 @@ var app = express();
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.get('/', function(req, res, next) {
-  res.redirect('/#!');
-})
 app.listen(process.env.PORT || 5000);
